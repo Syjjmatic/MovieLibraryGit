b2635c532e01fd18cde11d42f624cc28ec425c6f
Gary Olson <golsonmke@gmail.com>
2020-03-31 13:59:36 -0500

    Staged changes

--- Client/app.js
+++ Client/app.js
@@ -1,15 +1,11 @@
 (function($){
     function processForm( e ){
         var dict = {
-<<<<<<< HEAD
-            Title : this["title"].value,
-            Genre : this["genre"].value,
-        	Director: this["director"].value
-=======
+
         	Title : this["title"].value,
         	Director: this["director"].value,
-          Genre: this["genre"].value
->>>>>>> 34c52985da145364573062679d125b2fe2103ea8
+            Genre: this["genre"].value
+
         };
 
         $.ajax({
@@ -19,38 +15,15 @@
             contentType: 'application/json',
             data: JSON.stringify(dict),
             success: function( data, textStatus, jQxhr ){
-                $("response").html( JSON.stringify(dict) );
+                $('#response pre').html( data );
             },
             error: function( jqXhr, textStatus, errorThrown ){
                 console.log( errorThrown );
             }
         });
 
-        $.ajax({
-          url: 'https://localhost:44325/api/movie',
-          dataType: 'json',
-          type: 'get',
-          contentType: 'application/json',
-          data: JSON.stringify(dict),
-          success: function(){
-
-          },
-          error: function (jqXhr, textStatus, errorThrown){
-            console.log( errorThrown );
-          }
-
-
-        })
-
         e.preventDefault();
     }
 
     $('#my-form').submit( processForm );
 })(jQuery);
-<<<<<<< HEAD
-
-
-
-
-=======
->>>>>>> 34c52985da145364573062679d125b2fe2103ea8
