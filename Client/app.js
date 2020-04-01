91501345058679662eab949972327ef7d9dce94c
Gary Olson <golsonmke@gmail.com>
2020-04-01 16:26:28 -0500

    Completed ajax/jquery methods and did some html/ bootstrap.

--- Client/app.js
+++ Client/app.js
@@ -14,22 +14,20 @@
             type: 'post',
             contentType: 'application/json',
             data: JSON.stringify(dict),
-            before: function(data){
-                alert("Before Send Alert")
-            },
+            // before: function(data){
+            //     alert("Before Send Alert")
+            // },
             success: function( data, textStatus, jQxhr ){
                 $("#my-form input[type ='text']").val('');
                 alert("Success");
+                getMovies();
             },
-            error: function( jqXhr, textStatus, errorThrown ){
+            error: function( errorThrown ){
                 console.log( errorThrown );
             }
         });
-
         e.preventDefault();
-    
-
-    }
+       }
   
         function updateMovieDeets( e ){
             var movie = {
@@ -47,23 +45,79 @@
                 contentType: 'application/json',
                 data: JSON.stringify(movie),
                 
-                success: function( data, textStatus, jQxhr ){
+                success: function( data ){
                     alert("Success");
                     $("#edit-form input[type ='text']").val('');
-                   displayMovies();
+                   getMovies();
                 },
-                error: function( jqXhr, textStatus, errorThrown ){
+                error: function( errorThrown ){
                     console.log( errorThrown );
                 }
             });
     
             e.preventDefault();
         }
-       
+        $('#movies').click(getMovies);
         $('#my-form').submit( processForm );
         $('#edit-form').submit(updateMovieDeets);
     
        
-})(jQuery)
+})(jQuery);
+function getMovies(){
+    $.ajax({
+      url: 'https://localhost:44325/api/movie',
+      dataType: 'json',
+      type: 'get',
+      contentType: 'application/json',
+      success: function (data){
+        $("#movies").empty();
+        $.each(data, function(i,item){
+          var movie = "<tr>" +
+          "<td>" + item['title'] + "</td>" +
+          "<td>" + item['director'] + "</td>" +
+          "<td>" + item['genre'] + "</td>" +
+          "<td>" + "<button onclick=edit("+item['movieId']+") id='editMovie'>Edit</button>" + "</td>"+
+          "</tr>";
+          $("#movies").append(movie);
+
+        });
+        console.log(data);
+      },
+      failure: function(data){
+          alert(data.errorThrown)
+      },
+      error: function(data){
+          alert(data.errorThrown)
+      }
+    });
+  };
+  
+function getMovieDeets(id) {
+    $.ajax({
+        url: 'https://localhost:44325/api/movie',
+        dataType: 'json',
+        type: 'get',
+        contentType: 'application/json',
+        data: JSON.stringify(movie),
+        
+        success: function( data, textStatus, jQxhr ){
+            alert("Success");
+            $("#edit-form input[name ='movieId']").val(data['movieId']);
+            $("#edit-form input[type ='title']").val(data['title']);
+            $("#edit-form input[type ='genre']").val(data['genre']);
+            $("#edit-form input[type ='director']").val(data['director']);
+           
+        },
+        error: function(  errorThrown ){
+            console.log( errorThrown );
+        }
+    });
+
+    e.preventDefault();
+}
+
+
+
+
 
    
